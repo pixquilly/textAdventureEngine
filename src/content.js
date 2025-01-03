c58626bd42eba3439ae0aae95ccfd720c5ff509b
PixQuilly <pixquilly@gmail.com>
2025-01-03 19:57:56 +0200

    slight edit

--- src/content.js
+++ src/content.js
@@ -1,61 +1,44 @@
 let content = {
     "nodes":{
         0:{
-            "text":"this is the <span class='hl'>third</span> text. You can go back to previous texts, or choose other options that will reset this text.",
+            "text":"This is the first text.",
             "options":{
                 
-                1.1: {
-                    "text": "RECURSIVE",
-                    "goto": "0",
-                    "singleUse": true
-
-                },
                 1: {
-                    "text": "Go to 2.1",
-                    "goto": "2.1",
-                    "singleUse": true
+                    "text": "Continue",
+                    "goto": "1",
+                    "singleUse": false
+
+                }
+            }
+        },
+        1:{
+            "text":"This is the second text.",
+            "options":{
+                
+                1: {
+                    "text": "Go back.",
+                    "goto": "0",
+                    "singleUse": false
 
                 },
                 2: {
-                    "text": "Go to 2.2",
-                    "goto": "2.2",
-                    "singleUse": true
+                    "text": "Continue.",
+                    "goto": "2",
+                    "singleUse": false
 
                 }
             }
         },
-        2.1:{
-            "text":"this is 2.1",
+        2:{
+            "text":"This is the third and final text.",
             "options":{
                 
                 1: {
-                    "text": "Continue",
-                    "goto": "2.2",
-                    "singleUse": true
-
-                }
-            }
-        },
-        2.2:{
-            "text":"this is 2.2",
-            "options":{
-                
-                1: {
-                    "text": "Continue",
-                    "goto": "2.3",
-                    "singleUse": true
-
-                }
-            }
-        },
-        2.3:{
-            "text":"this is 2.3",
-            "options":{
-                
-                1: {
-                    "text": "Continue",
+                    "text": "Restart",
                     "goto": "0",
-                    "singleUse": true
+                    "singleUse": false
+
                 }
             }
         }
